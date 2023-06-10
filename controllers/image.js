ccffecf2ce90db98550abc55af6293e22b856f20
Ashok Choudhary <ashoka8769@gmail.com>
2023-06-10 07:29:50 +0530

    using grpc

--- controllers/image.js
+++ controllers/image.js
@@ -1,57 +1,45 @@
+const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
 
-const returnClarifaiRequestOptions = (imageUrl) => {
-    // Your PAT (Personal Access Token) can be found in the portal under Authentification
-    const PAT = "755086080ae04e1ca901539b6619fb53";
-    // Specify the correct user_id/app_id pairings
-    // Since you're making inferences outside your app's scope
-    const USER_ID = 'zl6yog5lellg';
-    const APP_ID = 'my-first-application';
-    // Change these to whatever model and image URL you want to use
+const stub = ClarifaiStub.grpc();
 
-    // const MODEL_ID = 'face-detection';
-    // const MODEL_VERSION_ID = 'aa7f35c01e0642fda5cf400f543e7c40';
-
-    const IMAGE_URL = imageUrl;
-
-    const raw = JSON.stringify({
-        "user_app_id": {
-            "user_id": USER_ID,
-            "app_id": APP_ID
-        },
-        "inputs": [
-            {
-                "data": {
-                    "image": {
-                        "url": IMAGE_URL
-                    }
-                }
-            }
-        ]
-    });
-
-    const requestOptions = {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Authorization': 'Key ' + PAT
-        },
-        body: raw
-    };
-
-    return requestOptions;
-
-}
-
+const metadata = new grpc.Metadata();
+metadata.set("authorization", "Key " + "755086080ae04e1ca901539b6619fb53");
 
 const handleApiCall = (req, res) => {
 
-    fetch("https://api.clarifai.com/v2/models/" + "face-detection" + "/outputs", returnClarifaiRequestOptions(req.body.input))
-        .then(response => response.json())
-        .then(data => {
-            res.json(data);
-        })
-        .catch(err => res.status(400).json("unable to work with API"));
+    stub.PostModelOutputs(
+        {
+            user_app_id: {
+                "user_id": "clarifai",
+                "app_id": "main"
+            },
+            model_id: "face-detection",
+            inputs: [
+                { data: { image: { url: req.body.input } } }
+            ]
+        },
+        metadata,
+        (err, response) => {
+            if (err) {
+                res.status(400).json("error fetching api");
+                throw new Error(err);
+                return;
+            }
 
+            if (response.status.code !== 10000) {
+                throw new Error("Post model outputs failed, status: " + response.status.description);
+                return;
+            }
+
+            // Since we have one input, one output will exist here.
+            const output = response.outputs[0];
+            // console.log("Predicted concepts:");
+            // for (const concept of output.data.concepts) {
+            //     console.log(concept.name + " " + concept.value);
+            // }
+            res.json(response)
+        }
+    );
 }
 
 const imageHandler = (req, res, db) => {
