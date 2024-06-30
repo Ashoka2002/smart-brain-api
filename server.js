dca52d9aaa2649a710fc7f587cba6f29f28cdc0a
Ashok Choudhary <ashoka8769@gmail.com>
2024-06-30 21:07:02 +0530

    adding faviocon

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
+require("dotenv").config();
 const express = require("express");
-const cors = require('cors');
-const bcrypt = require('bcrypt-nodejs');
-const knex = require('knex');
-const register = require('./controllers/register')
-const signin = require('./controllers/signin')
-const image = require('./controllers/image.js')
-const imageid = require('./controllers/imageId.js');
+const cors = require("cors");
+const bcrypt = require("bcrypt-nodejs");
+const knex = require("knex");
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const image = require("./controllers/image.js");
+const imageid = require("./controllers/imageId.js");
 const { port } = require("pg/lib/defaults");
 
-const db = knex({
-    client: 'pg',
-    connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl:{rejectUnauthorized:false},
-        host: process.env.DATABASE_HOST,
-        user: process.env.DATABASE_USER,
-        port: 5432,
-        password: process.env.DATABASE_PW,
-        database: process.env.DATABASE_DB
-    }
-});
+// const db = knex({
+//   client: "pg",
+//   connection: {
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: { rejectUnauthorized: false },
+//     host: process.env.DATABASE_HOST,
+//     user: process.env.DATABASE_USER,
+//     port: 5432,
+//     password: process.env.DATABASE_PW,
+//     database: process.env.DATABASE_DB,
+//   },
+// });
 
 const app = express();
 
@@ -27,16 +28,30 @@ app.use(cors());
 
 app.use(express.json());
 
-app.get('/', (req, res) => {res.send("Its working");})
+app.use("/favicon.ico", express.static("favicon.png"));
 
-app.post('/signin', signin.handlerSignin(db,bcrypt));
+app.get("/", (req, res) => {
+  res.send("Its working");
+});
 
-app.post('/register', (req, res) => {register.handlerRegister(req,res,db,bcrypt)})
+// app.post("/signin", signin.handlerSignin(db, bcrypt));
 
-app.get("/profile/:id", (req, res) =>{imageid.imageIdHandler(req,res,db)} );
+// app.post("/register", (req, res) => {
+//   register.handlerRegister(req, res, db, bcrypt);
+// });
 
-app.put('/image', (req, res) => {image.imageHandler(req,res,db)});
+// app.get("/profile/:id", (req, res) => {
+//   imageid.imageIdHandler(req, res, db);
+// });
 
-app.post('/imageurl', (req, res) => {image.handleApiCall(req,res)});
+// app.put("/image", (req, res) => {
+//   image.imageHandler(req, res, db);
+// });
 
-app.listen(process.env.PORT, () => {console.log(`app is running on port ${process.env.PORT}`);})
+// app.post("/imageurl", (req, res) => {
+//   image.handleApiCall(req, res);
+// });
+
+app.listen(process.env.PORT, () => {
+  console.log(`app is running on port http://localhost:${process.env.PORT}/`);
+});
